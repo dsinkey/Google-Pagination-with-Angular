4eb5dae1954a3659e57d9d7935a80986acfce901
Dustin <dustinsinkey1@gmail.com>
2016-01-26 23:35:44 -0800

    Fixed CSS and controller template

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -10,11 +10,11 @@ angular.module('myApp.controllers', []).controller('GooglePaginationCtrl', funct
           <td>\
             <td ng-repeat="page in pages track by $index" ng-click="selectPage(page.number)">\
             <span ng-if="page.active"><span class="csb active"></span>{{page.text}}</span>\
-            <a ng-if="!page.active"><span class="csb noactive"></span>{{page.text}}</a>\
+            <a ng-if="!page.active"><span class="csb nonactive"></span>{{page.text}}</a>\
           </td>\
           </td>\
           <td>\
-            <span ng-if="noNext()" class="csb noNext"></span>\
+            <span ng-if="noNext()" class="csb nonext"></span>\
               <a ng-if="!noNext()" ng-click="selectPage(page + 1)">\
             <span class="csb next"></span></a>\
           </td>\
