3798f932e879cb54f78de4438f7eed78c4101baf
Dustin <dustinsinkey1@gmail.com>
2016-01-26 17:36:39 -0800

    Updated CSS

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
 'use strict';
 
-angular.module('myApp', ['ui.bootstrap', 'ngRoute', 'myApp.controllers']).config(function($routeProvider) {  
-  $routeProvider.when('/issue/:issue', {templateUrl: 'views/view-issue.tpl.html'});
-	$routeProvider.otherwise({redirectTo: '/'});
-});
+angular.module('myApp', ['ui.bootstrap', 'myApp.controllers']);
